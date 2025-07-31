2c5c0458bb20a1c95959ee5d27d9930ab9f7d510
imba97 <mail@imba97.cn>
2025-07-31 23:49:56 +0800

    feat: content scripts loading mode

---
+++ src/contentScripts/components/open-image.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+onMounted(() => {
+  const video = document.querySelector('#playerWrap')
+  // eslint-disable-next-line no-console
+  console.log('playerWrap', video)
+})
+</script>
--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div absolute />
+  <div absolute>
+    <Component :is="component" v-for="(component, name) in components" :key="name" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { Component } from 'vue'
 import 'uno.css'
+
+const modules = import.meta.glob<{ default: Component }>('../components/*.vue', { eager: true })
+
+const components = Object.entries(modules).reduce<Record<string, Component>>((acc, [key, value]) => {
+  const name = key.split('/').pop()?.replace('.vue', '')
+  if (name) {
+    acc[name] = value.default
+  }
+  return acc
+}, {})
 </script>
